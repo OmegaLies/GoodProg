dd41b8e4209e5c7d408e93833b25830c93c3bbe9
OmegaLies <omegaunderwall@gmail.com>
2022-10-06 01:11:17 +0300

    Update balance.rb

--- Lesson5/balance.rb
+++ Lesson5/balance.rb
@@ -1,14 +1,14 @@
 # Получаем основные значения со ввода
 puts "Курс доллара:"
-rate = gets.chomp.to_f.round(2)
+rate = gets.to_f.round(2)
 puts "Сколько у вас рублей?"
-rubles = gets.chomp.to_f.round(2)
+rubles = gets.to_f.round(2)
 puts "Сколько у вас долларов?"
-dollars = gets.chomp.to_f.round(2)
+dollars = gets.to_f.round(2)
 
 # Переводим доллары в рубли и проверяем условие баланса в центах
 dollars_to_rubles = (dollars * rate).round(2)
-if (rubles - dollars_to_rubles).abs < 0.01
+if (rubles - dollars_to_rubles).abs < 0.01 * rate
 	puts "Ваш портфель сбалансирован!"
 elsif rubles > dollars_to_rubles
 	# Находим излишек валюты и продаем его для баланса
@@ -16,5 +16,5 @@ elsif rubles > dollars_to_rubles
 	puts "Вам нужно продать #{sell.round(2)}Р"
 else
 	sell = dollars_to_rubles - (rubles + dollars_to_rubles) / 2
-	puts "Вам нужно продать #{(sell / rate).round(4)}$"
-end	
+	puts "Вам нужно продать #{(sell / rate).round(2)}$"
+end
