edfaf9faecd19bb6468d1aca4f7fe413a97e5e13
OmegaLies <omegaunderwall@gmail.com>
2022-10-06 11:27:20 +0300

    Update balance2.rb

--- Lesson12/balance2.rb
+++ Lesson12/balance2.rb
@@ -1,13 +1,13 @@
 def balance(rate, rubles, dollars)
   dollars_to_rubles = (dollars * rate).round(2)
   if (rubles - dollars_to_rubles).abs <= 0.01 * rate
-    res = "Ваш портфель сбалансирован!"
+    res = [0 , true]
   elsif rubles > dollars_to_rubles
     sell = rubles - (rubles + dollars_to_rubles) / 2
-    res = "Вам нужно продать #{sell.round(2)}Р"
+    res = [sell.round(2), true]
   else
     sell = dollars_to_rubles - (rubles + dollars_to_rubles) / 2
-    res = "Вам нужно продать #{(sell / rate).round(2)}$"
+    res = [(sell / rate).round(2), false]
   end
   return res
 end
@@ -20,4 +20,14 @@ input_rubles = gets.to_f.round(2)
 puts "Сколько у вас долларов?"
 input_dollars = gets.to_f.round(2)
 
-puts balance(input_rate, input_rubles, input_dollars)
+result = balance(input_rate, input_rubles, input_dollars)
+output = "Вам нужно продать #{result[0]}"
+if 0.eql?(result[0])
+  output = "Ваш портфель сбалансирован!"
+elsif result[1]
+  output += "Р"
+else
+  output += "$"
+end
+
+puts output
