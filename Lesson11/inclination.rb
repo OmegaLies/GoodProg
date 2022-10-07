48780477740351dc3263bed7216fd23e8823de8c
OmegaLies <omegaunderwall@gmail.com>
2022-10-08 00:07:51 +0300

    Update inclination.rb

--- Lesson11/inclination.rb
+++ Lesson11/inclination.rb
@@ -1,16 +1,9 @@
 def inclination(num, form1, form2, form3)
-  num = num % 100
-  if num in 5..20
-    return form3
-  end
-  num = num % 10
-  if num == 1
-    form1
-  elsif num in 2..4
-    form2
-  else
-    form3
-  end
+  number %= 100
+  return form3 if number in 5..20
+  number %= 10
+  return form1 if number == 1
+  return form2 if number in 2..4
 end
 
 puts "Введите через пробел слово три формы слова (им. падеж, род. падеж, род падеж мн. числа):"
