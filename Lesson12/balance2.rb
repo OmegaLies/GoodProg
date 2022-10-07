691d9a90861fa6bc904f15b33e26c1b8bfc2e5c2
OmegaLies <omegaunderwall@gmail.com>
2022-10-07 18:07:38 +0300

    Update balance2.rb

--- Lesson12/balance2.rb
+++ Lesson12/balance2.rb
@@ -1,15 +1,14 @@
 def balance(rate, rubles, dollars)
   dollars_to_rubles = (dollars * rate).round(2)
   if (rubles - dollars_to_rubles).abs <= 0.01 * rate
-    res = [0 , true]
+    [-1 , true]
   elsif rubles > dollars_to_rubles
     sell = rubles - (rubles + dollars_to_rubles) / 2
-    res = [sell.round(2), true]
+    [sell.round(2), true]
   else
     sell = dollars_to_rubles - (rubles + dollars_to_rubles) / 2
-    res = [(sell / rate).round(2), false]
+    [(sell / rate).round(2), false]
   end
-  return res
 end
 
 # Получаем основные значения со ввода
@@ -22,7 +21,7 @@ input_dollars = gets.to_f.round(2)
 
 result = balance(input_rate, input_rubles, input_dollars)
 output = "Вам нужно продать #{result[0]}"
-if 0.eql?(result[0])
+if result[0] == -1
   output = "Ваш портфель сбалансирован!"
 elsif result[1]
   output += "Р"
