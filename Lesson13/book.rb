b1e476295ee07db8a084ebe8babab721b76d74d6
OmegaLies <omegaunderwall@gmail.com>
2022-10-10 01:54:34 +0300

    Update book.rb

--- Lesson13/book.rb
+++ Lesson13/book.rb
@@ -1,6 +1,27 @@
-class book
-  author
-  title
-  publish_date
+require 'date'
 
+class Book
+  def initialize(author, title, publish_date)
+    @author = author
+    @title = title
+    @publish_date = Date.parse(publish_date)
+  end
+
+  def author
+    return @author
+  end
+
+  def title
+    return @title
+  end
+
+  def publish_date
+    return @publish_date
+  end
 end
+
+a = Book.new("Артур Хейли", "Отель", "19.01.1990")
+b = Book.new("Братья Стругацкиe", "Пикник на обочине", "6.7.1978")
+
+puts "#{a.author} #{a.title} #{a.publish_date}"
+puts "#{b.author} #{b.title} #{b.publish_date}"
