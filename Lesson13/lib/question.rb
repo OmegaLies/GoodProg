19f77a393e20aa30230b752f636474fff9218fb2
OmegaLies <omegaunderwall@gmail.com>
2022-10-12 21:45:30 +0300

    Update question.rb

--- Lesson13/lib/question.rb
+++ Lesson13/lib/question.rb
@@ -1,27 +1,13 @@
 class Question
+  attr_reader :question, :answer, :score
+
   def initialize(question, answer, score)
     @question = question
     @answer = answer
     @score = score.to_i
   end
 
-  def question
-    return @question
-  end
-
-  def answer
-    return @answer
-  end
-
-  def score
-    return @score
-  end
-
   def right_answer?(user_answer)
-    if answer == user_answer
-      return true
-    else
-      return false
-    end
+    answer == user_answer
   end
 end
