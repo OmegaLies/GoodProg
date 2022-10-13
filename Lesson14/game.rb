fd89d146706b4b29484f4c051559ef8311d9a56f
OmegaLies <omegaunderwall@gmail.com>
2022-10-13 12:55:47 +0300

    Create game.rb

---
+++ Lesson14/game.rb
@@ -0,0 +1,41 @@
+require_relative "lib/film"
+
+file_names = Dir["#{__dir__}/data/*"]
+films = file_names.map do |file_name|
+  lines = File.readlines(file_name, chomp: true)
+  Film.new(lines[0], lines[1], lines[2])
+end
+
+number_of_questions = 3
+right_answers = 0
+
+directors = []
+films.each do |film|
+  director = film.director
+  unless directors.include?(director)
+    directors << director
+  end
+end
+
+films.sample(number_of_questions).each do |film|
+  puts "Кто снял фильм «#{film.title}»?"
+  puts
+
+  answers = [film.director] + (directors - [film.director]).sample(3).map { |variant| variant }
+
+  answers.shuffle!.each_with_index { |answer, index| puts "#{index + 1}. #{answer}" }
+  puts
+
+  user_input = STDIN.gets.to_i
+  if film.right_answer?(answers[user_input - 1])
+    puts "Верно!"
+    puts
+    right_answers += 1
+  else
+    puts "Неверно! Правильный ответ - #{film.director}"
+    puts
+  end
+end
+
+puts "Правильных ответов: #{right_answers} из #{number_of_questions}"
+puts
