335b03f47fc9e89fe7418a68bbfaba6ae32c6622
OmegaLies <omegaunderwall@gmail.com>
2022-10-13 17:11:00 +0300

    Update film_night.rb

--- Lesson15/film_night.rb
+++ Lesson15/film_night.rb
@@ -8,3 +8,20 @@ end
 
 directors = films.map { |film| film.director }
 directors.uniq
+
+puts "Программа «Фильм на вечер»"
+puts
+puts "Фильм какого режисера вы хотите сегодня посмотреть?"
+puts
+puts "Список режисеров:"
+
+directors.each_with_index { |director, index| puts "#{index + 1}. #{director}" }
+puts
+puts "Введите номер из списка:"
+user_input = STDIN.gets.to_i
+puts
+
+diretor_films = films.find_all { |film| film.director == directors[user_input - 1] }
+
+puts "Сегодня вечером рекомендую посмотреть:"
+puts diretor_films.sample.full_info
