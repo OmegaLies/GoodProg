6799a41b12f087ac1e5f9c05b40476ed05253110
OmegaLies <omegaunderwall@gmail.com>
2022-10-26 09:32:59 +0300

    Update happy_birthday.rb

--- Lesson25/happy_birthday.rb
+++ Lesson25/happy_birthday.rb
@@ -1,14 +1,26 @@
 # encoding: utf-8
 
 def happy_birthday(person)
-  "Дорогой #{person[:name]}\n\n"\
-  "Поздравляем Вас с днём рождения!\n"\
-  "Вам сегодня исполняется аж #{person[:age]}\n\n"\
-  "#{person[:text]}\n"
+  greeting =
+    case person[:sex]
+    when "Male" then "Дорогой"
+    when "Female" then "Дорогая"
+    else
+      "Дорогое"
+    end
+  <<HEREDOC
+  #{greeting} #{person[:name]}
+
+  Поздравляем Вас с днём рождения!
+  Вам сегодня исполняется аж #{person[:age]}
+
+  #{person[:text]}
+HEREDOC
 end
 
 person = {
   name: "Борис",
+  sex: "Male",
   age: "48",
   text: "Желаем успехов в лечении простатита!"
 }
