5c06d5680a170dfd59d38f2386257b85aab19a76
OmegaLies <omegaunderwall@gmail.com>
2022-11-06 19:39:03 +0300

    Style edits in tests for film

--- Lesson15/spec/film_night_spec.rb
+++ Lesson15/spec/film_night_spec.rb
@@ -1,19 +1,22 @@
 #encoding: UTF-8
 
-require "rspec"
-require_relative "../lib/film"
+require "film"
 
 describe Film do
-  it "Возвращается название" do
-    expect(Film.new("Фанатик", "Генри Бин", 2001,).title).to eq "Фанатик"
+  let(:film) { Film.new("Фанатик", "Генри Бин", 2001,) }
+  describe "#name" do
+    it { expect(film.title).to eq "Фанатик" }
   end
-  it "Возвращается режиссер" do
-    expect(Film.new("Шум", "Генри Бин", 2007,).director).to eq "Генри Бин"
+
+  describe "#title" do
+    it { expect(film.director).to eq "Генри Бин" }
   end
-  it "Возвращается год" do
-    expect(Film.new("Хористы", "Кристоф Барратье", 2004,).year).to eq 2004
+
+  describe "#year" do
+    it { expect(film.year).to eq 2001 }
   end
-  it "Возвращается полная информация" do
-    expect(Film.new("Зеленая миля", "Фрэнк Дарабонт", 1999,).full_info).to eq "Фрэнк Дарабонт - Зеленая миля - (1999)"
+
+  describe "#full_info" do
+    it { expect(film.full_info).to eq "Генри Бин - Фанатик - (2001)" }
   end
 end
